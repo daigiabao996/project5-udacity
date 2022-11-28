7278dc5be47ea499eb6677addea2f72379d360f4
daigiabao996 <vanquocquanbao96@gmail.com>
2022-11-28 17:04:23 +0700

    update

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1 style='text-align: center; font-size: 50px;'>Hello, This is BaoVQQ!</h1>"
-if __name__ == '__main__':hjhjjj
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
